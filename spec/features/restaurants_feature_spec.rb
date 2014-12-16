d129c2323c33918a6a7145e3eb70f19d9c28d989
Richard Lake <ralake1985@gmail.com>
2014-12-16 12:28:05 +0000

    add model and migrate

--- spec/features/restaurants_feature_spec.rb
+++ spec/features/restaurants_feature_spec.rb
@@ -1,11 +1,28 @@
 require 'rails_helper'
 
 feature 'restaurants' do
+
 	context 'no restaurants have been added' do
 		scenario 'should display a prompt to add a restaurant' do
 			visit '/restaurants'
 			expect(page).to have_content 'No restaurants'
 			expect(page).to have_link 'Add a restaurant'
 		end
+
 	end
+
+	context 'restaurants have been added' do
+
+		before do
+			Restaurant.create(name: 'Crispys Palace')
+		end
+
+		scenario 'display restaurants' do
+			visit '/restaurants'
+			expect(page).to have_content('Crispys Palace')
+			expect(page).not_to have_content('No restaurants yet')
+		end
+
+	end
+
 end
