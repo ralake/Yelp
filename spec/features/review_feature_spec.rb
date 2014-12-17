67adc4f48c5e487aa9f605172de1885290f6dc99
Richard Lake <ralake1985@gmail.com>
2014-12-17 08:31:16 +0000

    Pass test to check reviews are deleted once their parent restaurant is removed

--- spec/features/review_feature_spec.rb
+++ spec/features/review_feature_spec.rb
@@ -10,9 +10,20 @@ feature 'reviewing' do
 		fill_in 'Gripes', with: "scary, but rather marvelous music"
 		select '4', from: 'Rating'
 		click_button 'Leave Review'
-		save_and_open_page
 		expect(current_path).to eq '/restaurants'
 		expect(page).to have_content("scary, but rather marvelous music")
+	end
+
+	scenario 'removes reviews when restaurants are deleted' do
+		visit '/restaurants'
+		click_link 'Review Richies Cantina'
+		fill_in 'Gripes', with: "scary, but rather marvelous music"
+		select '4', from: 'Rating'
+		click_button 'Leave Review'
+		click_link 'Delete Richies Cantina'
+		expect(current_path).to eq '/restaurants'
+		expect(page).not_to have_content("scary, but rather marvelous music")
+		save_and_open_page
 	end
 
 end
