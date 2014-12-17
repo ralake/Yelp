cd30400d12ac641be1741685b5a3a5d53b3f8b4a
Richard Lake <ralake1985@gmail.com>
2014-12-17 17:30:53 +0000

    Add test to make sure only authors can edit their reviews

--- spec/features/restaurants_feature_spec.rb
+++ spec/features/restaurants_feature_spec.rb
@@ -9,6 +9,12 @@ def user_signin
 	click_button('Sign up')
 end
 
+def add_restaurant
+	click_link 'Add a restaurant'
+	fill_in 'Name', with: 'Crispys Palace'
+	click_button 'Create Restaurant'
+end
+
 feature 'restaurants' do
 
 	context 'no restaurants have been added' do
@@ -36,9 +42,7 @@ feature 'restaurants' do
 
 		scenario 'prompt user to fill out a form, then displays new restaurant' do
 			user_signin
-			click_link 'Add a restaurant'
-			fill_in 'Name', with: 'Crispys Palace'
-			click_button 'Create Restaurant'
+			add_restaurant
 			expect(page).to have_content 'Crispys Palace'
 			expect(current_path).to eq '/restaurants'
 		end
@@ -50,6 +54,11 @@ feature 'restaurants' do
 			click_button 'Create Restaurant'
 			expect(page).not_to have_css 'h2', text: 'cb'
 			expect(page).to have_content 'error'
+		end
+
+		it "can only have a restaurant added by a signed in user" do
+			visit ('/')
+			expect(page).not_to have_link('Add a restaurant')
 		end
 
 	end
@@ -69,15 +78,26 @@ feature 'restaurants' do
 
 	context 'editing restaurants' do
 
-		before { Restaurant.create name:'Crispys Palace' }
-
 		scenario 'let a user edit a restaurant' do
 			user_signin
+			add_restaurant
 			click_link 'Edit Crispys Palace'
 			fill_in 'Name', with: 'Crispys Hovel'
 			click_button 'Update Restaurant'
 			expect(page).to have_content 'Crispys Hovel'
 			expect(current_path).to eq '/restaurants'
+		end
+
+		scenario 'it will not allow a user to edit a restaurant that they did not create' do
+			user_signin
+			add_restaurant
+			click_link('Sign out')
+			click_link('Sign up')
+			fill_in('Email', with: 'rich@example.com')
+			fill_in('Password', with: 'testrich')
+			fill_in('Password confirmation', with: 'testrich')
+			click_button('Sign up')
+			expect(page).not_to have_link('Edit Crispys Palace')
 		end
 
 	end
