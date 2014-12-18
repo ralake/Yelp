8685e633db43b197151b00eb085e750cbb07f61c
Richard Lake <ralake1985@gmail.com>
2014-12-18 19:33:18 +0000

    Ajax funtionality finished

--- spec/features/endorsements_feature_spec.rb
+++ spec/features/endorsements_feature_spec.rb
@@ -7,13 +7,13 @@ feature 'endorsing reviews' do
 		kfc.reviews.create(rating: 3, gripes: 'it was absolutely revolting')
 	end
 
-	scenario 'a user can endorse a review, which updates the review endorsement count' js: true do
+	scenario 'a user can endorse a review, which updates the review endorsement count', js: true do
 		visit '/'
 		click_link 'Endorse'
 		expect(page).to have_content('1 endorsement')
 	end
 
-	scenario 'multiple endorsements' do
+	scenario 'multiple endorsements', js: true do
 		visit '/'
 		3.times { click_link 'Endorse'}
 		expect(page).to have_content('3 endorsements')
