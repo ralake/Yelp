926888cd79e52ace19c9dabf8c13faf92462aafa
Richard Lake <ralake1985@gmail.com>
2014-12-18 17:56:11 +0000

    Add endoresements

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,11 @@ Rails.application.routes.draw do
 	root to: "restaurants#index"
 
   devise_for :users, :controllers => {:omniauth_callbacks => "users/omniauth_callbacks"}
-  resources :restaurants do
-  	resources :reviews
+
+  resources :restaurants, shallow: true do
+  	resources :reviews do
+  		resources :endorsements
+  	end
   end
 
 end
---
+++ spec/features/endorsements_feature_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+feature 'endorsing reviews' do
+	
+	before do
+		kfc = Restaurant.create(name: 'KFC')
+		kfc.reviews.create(rating: 3, gripes: 'it was absolutely revolting')
+	end
+
+	scenario 'a user can endorse a review, which updates the review endorsement count' do
+		visit '/'
+		click_link 'Endorse KFC'
+		expect(page).to have_content('1 endorsement')
+	end
+
+end
